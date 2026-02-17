4168652c4f043a5cb147926fb3f903bbfe1fdad0
Retr0581 <getmandima600@gmail.com>
2026-02-17 23:56:22 +0300

    Add: смена кодировки

--- main.c
+++ main.c
@@ -77,19 +77,19 @@ long double generate_number(long double a, long double b, int p) {
 
 void generate_variant(int variant, int k, int bits, long double a, long double b, int p) {
     char filename1[100], filename2[100];
-    sprintf(filename1, "Задания/вариант_%d.md", variant);
-    sprintf(filename2, "Проверка/вариант_%d.md", variant);
+    sprintf(filename1, "пїЅпїЅпїЅпїЅпїЅпїЅпїЅ/пїЅпїЅпїЅпїЅпїЅпїЅпїЅ_%d.md", variant);
+    sprintf(filename2, "пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ/пїЅпїЅпїЅпїЅпїЅпїЅпїЅ_%d.md", variant);
     
     FILE *f_task = fopen(filename1, "w");
     FILE *f_check = fopen(filename2, "w");
     
     if (!f_task || !f_check) {
-        printf("Ошибка создания файла!");
+        printf("пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ!");
         return;
     }
     
-    fprintf(f_task, "№ п/п | Вещественное число\n");
-    fprintf(f_check, "№ п/п | Вещественное число | Машинное представление | Точность\n");
+    fprintf(f_task, "пїЅ пїЅ/пїЅ | пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ\n");
+    fprintf(f_check, "пїЅ пїЅ/пїЅ | пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ | пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ | пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ\n");
     
     for (int i = 1; i <= k; i++) {
         long double original = generate_number(a, b, p);
@@ -113,7 +113,7 @@ int main() {
     
     FILE *config = fopen("config.txt", "r");
     if (!config) {
-        printf("Ошибка: файл config.txt не найден!\n");
+        printf("пїЅпїЅпїЅпїЅпїЅпїЅ: пїЅпїЅпїЅпїЅ config.txt пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ!\n");
         return 1;
     }
     
@@ -129,15 +129,15 @@ int main() {
     
     fclose(config);
     
-    printf("ГЕНЕРАТОР ИНДИВИДУАЛЬНЫХ ЗАДАНИЙ\n");
-    printf("Вариантов: %d\n", n);
-    printf("Заданий: %d\n", k);
-    printf("Разрядность: %d бит\n", bits);
-    printf("Диапазон: [%.2Lf, %.2Lf]\n", a, b);
-    printf("Знаков после запятой: %d\n", p);
+    printf("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ\n");
+    printf("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ: %d\n", n);
+    printf("пїЅпїЅпїЅпїЅпїЅпїЅпїЅ: %d\n", k);
+    printf("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ: %d пїЅпїЅпїЅ\n", bits);
+    printf("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ: [%.2Lf, %.2Lf]\n", a, b);
+    printf("пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ: %d\n", p);
     
-    mkdir("Задания");
-    mkdir("Проверка");
+    mkdir("пїЅпїЅпїЅпїЅпїЅпїЅпїЅ");
+    mkdir("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ");
     
     for (int variant = 1; variant <= n; variant++) {
         generate_variant(variant, k, bits, a, b, p);
